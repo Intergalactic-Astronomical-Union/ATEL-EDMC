b8e0232aa3af423fc16b34c78de03e47ea123a4d
Intergalactic Astronomical Union <56673950+Elite-IGAU@users.noreply.github.com>
2019-10-23 17:19:26 -0700

    Update load.py

--- ATEL/load.py
+++ ATEL/load.py
@@ -18,10 +18,10 @@ from collections import defaultdict
 import threading
 import urllib2
 
-VERSION = '0.02a'
-
 this = sys.modules[__name__]	# For holding module globals
 status = tk.StringVar()
+VERSION = '0.02a'
+this.github = "https://raw.githubusercontent.com/Elite-IGAU/ATEL-EDMC/master/ATEL/load.py"
 
 def plugin_start(plugin_dir):
     """
@@ -39,10 +39,43 @@ def plugin_prefs(parent):
                    url='https://github.com/Elite-IGAU/ATEL-EDMC', underline=True).grid(columnspan=2, padx=PADX, sticky=tk.W)
     nb.Label(frame, text="ATEL {VER}".format(
         VER=VERSION)).grid(columnspan=2, padx=PADX, sticky=tk.W)
+    nb.Label(frame).grid()  # spacer
+    nb.Button(frame, text="UPGRADE", command=upgrade_callback).grid(row=10, column=0,
+        columnspan=2, padx=PADX, sticky=tk.W)
     #
     # Todo - Add option to defer ATEL transmission until docked and cartographic data is sold.
     #
     return frame
+
+def upgrade_callback():
+    this_fullpath = os.path.realpath(__file__)
+    this_filepath, this_extension = os.path.splitext(this_fullpath)
+    corrected_fullpath = this_filepath + ".py"
+    try:
+        response = requests.get(this.github)
+        if (response.status_code == 200):
+            with open(corrected_fullpath, "wb") as f:
+                f.seek(0)
+                f.write(response.content)
+                f.truncate()
+                f.flush()
+                os.fsync(f.fileno())
+                this.upgrade_applied = True  # Latch on upgrade successful
+                msginfo = ['Upgrade has completed sucessfully.',
+                           'Please close and restart EDMC']
+                tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
+            sys.stderr.write("Finished plugin upgrade!\n")
+
+        else:
+            msginfo = ['Upgrade failed. Bad server response',
+                       'Please try again']
+            tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
+    except:
+        sys.stderr.writelines(
+            "Upgrade problem when fetching the remote data: {E}\n".format(E=sys.exc_info()[0]))
+        msginfo = ['Upgrade encountered a problem.',
+                   'Please try again, and restart if problems persist']
+        tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
 
 def plugin_app(parent):
     """
