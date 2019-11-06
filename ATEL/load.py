61db3999b6a716e406e3164e366ba9b641186ab0
Elite-IGAU <rjs.social@raymondjsanders.com>
2019-11-05 19:24:42 -0700

    Update load.py
    Code clean up, and preparation for 1.0 release.

--- ATEL/load.py
+++ ATEL/load.py
@@ -21,14 +21,16 @@
 # DATA COLLECTION NOTICE:
 # IGAU only stores the following data:
 # discovery date/time, discovery name, and discovery location.
+# IGAU does NOT store any "Personally Identifiable Information" (PII) in our
+# public discovery catalog.
 #
 # Please submit bug reports or issues at:
 # https://github.com/Elite-IGAU/ATEL-EDMC/issues
 #
 # Special thanks to:
 #
-# Mobius PVE (https://elitepve.com/) for their assistance
 # Otis B. EDMC (http://edcodex.info/?m=tools&entry=150) for EDMC plugin docs
+# (DISC)Sajime and (Mobius) Odyssey for their assistance and suggestions.
 #
 # The many wonderful explorers that make the Intergalactic Astronomical Union
 # [IGAU] a productive and enjoyable Elite Dangerous PVE squadron.
@@ -55,7 +57,7 @@ import time
 
 this = sys.modules[__name__]	# For holding module globals
 status = tk.StringVar()
-VERSION = '0.70b'
+VERSION = '1.0-RC1'
 IGAU_GITHUB = "https://raw.githubusercontent.com/Elite-IGAU/ATEL-EDMC/latest/ATEL/load.py"
 IGAU_API = "https://ddss70885k.execute-api.us-west-1.amazonaws.com/Prod"
 IGAU_WIKI = "https://elite-dangerous-iau.fandom.com/api.php"
@@ -72,42 +74,43 @@ def plugin_prefs(parent):
     HyperlinkLabel(frame, text='ATEL GitHub', background=nb.Label().cget('background'),
                    url='https://github.com/Elite-IGAU/ATEL-EDMC/tree/latest', underline=True).grid(columnspan=2, padx=PADX, sticky=tk.W)
     nb.Label(frame, text="ATEL {VER}".format(VER=VERSION)).grid(columnspan=2, padx=PADX, sticky=tk.W)
-    nb.Button(frame, text="UPGRADE", command=upgrade_callback).grid(row=10, column=0,
-        columnspan=2, padx=PADX, sticky=tk.W)
     return frame
 
 def upgrade_callback():
-    this_fullpath = os.path.realpath(__file__)
-    this_filepath, this_extension = os.path.splitext(this_fullpath)
-    corrected_fullpath = this_filepath + ".py"
-    try:
-        response = requests.get(IGAU_GITHUB)
-        if (response.status_code == 200):
-            with open(corrected_fullpath, "wb") as f:
-                f.seek(0)
-                f.write(response.content)
-                f.truncate()
-                f.flush()
-                os.fsync(f.fileno())
-                this.upgrade_applied = True  # Latch on upgrade successful
-                msginfo = ['Upgrade has completed sucessfully.',
-                           'Please close and restart EDMC']
-                tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
+    #
+    # disabled - pending rewite to do this automagically.
+    #
+    #this_fullpath = os.path.realpath(__file__)
+    #this_filepath, this_extension = os.path.splitext(this_fullpath)
+    #corrected_fullpath = this_filepath + ".py"
+    #try:
+    #    response = requests.get(IGAU_GITHUB)
+    #    if (response.status_code == 200):
+    #        with open(corrected_fullpath, "wb") as f:
+    #            f.seek(0)
+    #            f.write(response.content)
+    #            f.truncate()
+    #            f.flush()
+    #            os.fsync(f.fileno())
+    #            this.upgrade_applied = True  # Latch on upgrade successful
+    #            msginfo = ['Upgrade has completed sucessfully.',
+    #                       'Please close and restart EDMC']
+    #            tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
 
-        else:
-            msginfo = ['Upgrade failed. Bad server response',
-                       'Please try again']
-            tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
-    except:
-        msginfo = ['Upgrade encountered a problem.',
-                   'Please try again, and restart if problems persist']
-        tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
+    #    else:
+    #        msginfo = ['Upgrade failed. Bad server response',
+    #                   'Please try again']
+    #        tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
+    #except:
+    #    msginfo = ['Upgrade encountered a problem.',
+    #               'Please try again, and restart if problems persist']
+    #    tkMessageBox.showinfo("Upgrade status", "\n".join(msginfo))
 
 def dashboard_entry(cmdr, is_beta, entry):
     this.cmdr = cmdr
 
 def bulletin_callback():
-    # Looks like the only way to get this to work is to embrce the icky JSON fad.
+    # I guess the only way to get this to work is to embrace the icky JSON fad.
     ATEL_DATA = {
         'action': 'edit',
         'title': 'GBET '+str(jd)+': '+this.system,
@@ -131,28 +134,26 @@ def plugin_app(parent):
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
 #############################
-# What we're really after are unique discoveries.
+# What we're really after are unique discoveries. TODO: Filter out "mundane"
+# CodexEntry events for stuff like Y-Type Dwarfs, HMC's, etc. These will generate
+# a CodexEntry event the first time a CMDR encounters one in a sector.
+#
+# Not an urgent fix because the data isn't entirely worthless.
+# (Black Holes, ELW's, WD/NS Herbigs, CS, are of interest.)
 
     if entry['event'] == 'CodexEntry':
         # We discovered something!
             this.cmdr = cmdr
             entry['commanderName'] = cmdr
-
-            # We don't need to announce discoveries to the client - should work quietly in the background.
-            #status.set("{}: Discovered {} in {}\n".format(entry['timestamp'],entry['Name_Localised'],entry['System']))
-            status.set("Discover something interesting? \n  Click the button below to report it!")
-
-            # data to be sent to api
+            # send journal strings to database endpoint:
             this.name=(format(entry['Name_Localised']))
             this.system=(format(entry['System']))
             this.timestamp=(format(entry['timestamp']))
             DATA_STR = '{{ "timestamp":"{}", "Name_Localised":"{}", "System":"{}" }}'.format(entry['timestamp'], entry['Name_Localised'], entry['System'])
             r = requests.post(url = IGAU_API, data = DATA_STR)
 
-            # extracting response text
-            db_response = r.text
-
-            #Submit ATEL Button
+            #Submit ATEL Button if CMDR wants to make a public discovery announcement.
+            status.set("Discover something interesting? \n  Click the button below to report it!")
             nb.Button(frame, text="[Transmit ATEL]", command=bulletin_callback).grid(row=10, column=0,
             columnspan=2, padx=PADX, sticky=tk.W)
     else:
@@ -161,12 +162,15 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
                 # We arrived at a new system!
                     this.cmdr = cmdr
                     entry['commanderName'] = cmdr
-
-                    status.set("Discover something interesting? \n  Click the button below to report it!")
                     this.system=(format(entry['StarSystem']))
                     this.timestamp=(format(entry['timestamp']))
+                    # Since we don't know what "name" is unless we have a
+                    # CodexEntry event, we use "Unknown Entity" as a generic
+                    # discovery type for systems with unsual or unknown phenomena
                     this.name = 'Unknown Entity'
 
+                    #Submit ATEL Button if CMDR wants to make a public discovery announcement.
+                    status.set("Discover something interesting? \n  Click the button below to report it!")
                     nb.Button(frame, text="[Transmit ATEL]", command=bulletin_callback).grid(row=10, column=0,
                     columnspan=2, padx=PADX, sticky=tk.W)
 
