5dfba892c7ca6c858fe19ede15fef3c2a767cf76
Elite-IGAU <>
2019-12-22 17:43:05 -0700

    version 1.09 final
    updated Submit ATEL Report Button:
    
    - Submit ATEL button is now displayed when a CodexEntry event with a value greater than 0 credits occurs. This should reduce ATEL reports from within the bubble.
    
    - Submit ATEL button is now cleared from the main window upon FSDJump event
    
    - Updated main status window text
    
    - removed duplicate variables.

--- ATEL/load.py
+++ ATEL/load.py
@@ -56,9 +56,7 @@ import time
 
 this = sys.modules[__name__]	# For holding module globals
 this.status = tk.StringVar()
-this.ts = time.time()
-this.jd = this.ts / 86400 + 2440587.5
-VERSION = '1.12'
+VERSION = '1.09'
 IGAU_GITHUB = "https://raw.githubusercontent.com/Elite-IGAU/ATEL-EDMC/latest/ATEL/load.py"
 IGAU_GITHUB_LATEST_VERSION = "https://raw.githubusercontent.com/Elite-IGAU/ATEL-EDMC/latest/ATEL/version.txt"
 IGAU_API = "https://ddss70885k.execute-api.us-west-1.amazonaws.com/Prod"
@@ -117,7 +115,7 @@ def bulletin_callback():
     # update the current time, otherwise ATEL notices will all have the same timestamp
     this.ts = time.time()
     this.jd = this.ts / 86400 + 2440587.5
-    # I guess the only way to get this to work is to embrace the icky JSON fad.
+    # Fandom wiki API needs icky JSON.
     ATEL_DATA = {
         'action': 'edit',
         'title': 'GBET '+str(jd)+': '+this.system,
@@ -125,9 +123,16 @@ def bulletin_callback():
         'token': '+\\',
         'format': 'json'
     }
-
     ATEL_POST = requests.post(IGAU_WIKI, data=ATEL_DATA)
     this.status.set("ATEL "+str(jd)+" Transmitted")
+    # We don't issue forget(this.b1) in case there are multiple CodexEvents to report.
+    # FSDJump event will clear the button.
+
+def forget(widget):
+    widget.grid_forget()
+
+def retrieve(widget):
+    widget.grid(row=10, column=0, columnspan=2, padx=PADX)
 
 def plugin_app(parent):
     this.parent = parent
@@ -135,37 +140,32 @@ def plugin_app(parent):
     this.frame.columnconfigure(2, weight=1)
     this.lblstatus = tk.Label(this.frame, anchor=tk.W, textvariable=status, wraplengt=255)
     this.lblstatus.grid(row=0, column=1, sticky=tk.W)
-    this.status.set("ATEL-EDMC (Version "+VERSION+") Loaded \n Waiting for Codex entry or composition scan")
+    this.status.set("Waiting for Codex discovery data...")
     return this.frame
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
-#############################
-# What we're really after are unique discoveries.
+
     if entry['event'] == 'CodexEntry':
         # We discovered something!
+            # declare variables
+            this.timestamp=(format(entry['timestamp']))
             this.cmdr = cmdr
             entry['commanderName'] = cmdr
-            # send journal strings to database endpoint:
             this.name=(format(entry['Name_Localised']))
             this.system=(format(entry['System']))
-            this.timestamp=(format(entry['timestamp']))
+            # assemble icky JSON
             DATA_STR = '{{ "timestamp":"{}", "Name_Localised":"{}", "System":"{}" }}'.format(entry['timestamp'], entry['Name_Localised'], entry['System'])
+            # send journal strings to database endpoint:
             r = requests.post(url = IGAU_API, data = DATA_STR)
-            #
             # Submit ATEL Button if CMDR wants to make a public discovery announcement.
-            # Updated wording to be more clear as to what people should be submitting.
-            # Also added a value check - unless a CodexEntry event generates a Voucher from a composition scan, we don't offer the report button.
-            #
+            # Added a value check - unless a CodexEntry event generates a Voucher from a composition scan, we don't offer the report button.
             try:
                 this.voucher=(format(entry['VoucherAmount']))
-                this.status.set("Codex entry transmitted.\n "+this.name+"\n"+"[Submit Atel Report - Disabled]")
-                #
-                # nb.button might have to be moved to its own function. Also
-                # grid(row=10, column=0, columnspan=2, padx=PADX, sticky=tk.W) may need to be on a seperate line/function
-                #
-                #nb.Button(frame, text="[Submit ATEL Report]", command=bulletin_callback).grid(row=10, column=0, columnspan=2, padx=PADX, sticky=tk.W)
+                this.status.set("Codex discovery data sent.\n "+this.name+"")
+                this.b1 = nb.Button(frame, text="[Submit ATEL Report?]", command=bulletin_callback)
+                retrieve(this.b1)
             except KeyError:
-                this.status.set("Codex entry transmitted.\n "+this.name)
+                this.status.set("Codex discovery data sent.\n "+this.name)
     else:
         # FSDJump happens often enough to clear the status window
             if entry['event'] == 'FSDJump':
@@ -175,10 +175,11 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
                     this.timestamp=(format(entry['timestamp']))
                     this.population=(format(entry['Population']))
                     population = int (this.population)
-                    #
-                    # need a function call or something here to remove nb.button (the submit ATEL button) if it exists.
-                    #
-                    this.status.set("Waiting for Codex entry or composition scan")
+                    this.status.set("Waiting for Codex discovery data...")
+                    try:
+                        forget(this.b1)
+                    except AttributeError:
+                        this.status.set("Waiting for Codex discovery data...")
 
 def plugin_stop():
     sys.stderr.write("Shutting down.")
